6c477d688f9b916badaf222bd24eaa030de009fc
Jason McCandless <jasonmccandless@gmail.com>
2011-03-23 20:53:58 +0000

    Make it actually useful to others.

--- luas.py
+++ luas.py
@@ -3,39 +3,55 @@
 from BeautifulSoup import BeautifulSoup
 import urllib
 import datetime
+import getopt
+import sys
+
+def usage():
+	print "luas.py [-i] [-o] [-l location]"
 
 
 
-webpage = urllib.urlopen('http://www.luas.ie/luaspid.html?get=Abbey+Street')
-data = webpage.read()
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv, "oil:")
+	except getopt.GetoptError:           
+		usage()
+		sys.exit(2)
+
+	location = "Abbey+Street"
+	direction = 'outgoing'
+	for opt, arg in opts:
+		if opt == '-i':
+			direction = 'incoming'
+		elif opt == "-l":
+			location = arg
 
 
-soup = BeautifulSoup(data)
+	webpage = urllib.urlopen('http://www.luas.ie/luaspid.html?get=%s'%location)
+	data = webpage.read()
+	soup = BeautifulSoup(data)
+	incomingDiv = soup.find("div","Inbound")
+	outgoingDiv = incomingDiv.findNextSibling('div')
 
-
-incomingDiv = soup.find("div","Inbound")
-
-
-outgoingDiv = incomingDiv.findNextSibling('div')
-
-
-k = []
-for x in outgoingDiv.findAll('div'):
-	k.append(x.contents[0])
-
-
-outgoingTimes = zip(k[::2], k[1::2])
-
-
-for t in outgoingTimes:
-	dest = t[0]
-	mins = t[1]
-	
-
-	if mins.isdigit():
-		esttime = (datetime.datetime.now() + datetime.timedelta(minutes=int(mins))).strftime("%I:%M %p")
+	if direction == 'incoming':
+		directionDiv = incomingDiv
 	else:
-		esttime = ""
+		directionDiv = outgoingDiv
 
-	print dest,mins,'\t',esttime
+	divcontents = []
+	for x in directionDiv.findAll('div'):
+		divcontents.append(x.contents[0])
+
+	#pair the destination and the ETA
+	outgoingTimes = zip(divcontents[::2], divcontents[1::2])
+
+	for dest,mins in outgoingTimes:
+		if mins.isdigit():
+			esttime = (datetime.datetime.now() + datetime.timedelta(minutes=int(mins))).strftime("%I:%M %p")
+		else:
+			esttime = ""
+		print dest,mins,'\t',esttime
 	
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
